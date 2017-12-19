2a004f200b16757a5ede835116e8cc06c8495bbf
Alex <acharlot91@gmail.com>
2017-12-19 12:10:44 +0100

    Adding template motor

--- core/View.class.php
+++ core/View.class.php
@@ -4,8 +4,8 @@ class view {
   private $t;
 
   public function __construct ($v = 'default', $t = 'front') {
-    $this->v = $v.'view.php';
-    $this->t = $t.'tpl.php';
+    $this->v = $v.'.view.php';
+    $this->t = $t.'.tpl.php';
 
     if (!file_exists('views/'.$this->$v)) {
       die('Le template: '.$this->$v.'n\'existe pas');
@@ -14,5 +14,9 @@ class view {
     if (!file_exists('views/templates/'.$this->$t)) {
       die('Le template: '.$this->$t.'n\'existe pas');
     }
+  }
+
+  public function __destruct () {
+    include 'views/templates/'.$this->t;
   }
 }
